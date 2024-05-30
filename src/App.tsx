a2cc8e325bafd0eeb2931047bfb45a3ff14444bd
Jonas <polyfant@live.se>
2024-05-30 04:04:56 +0200

    changed over to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import './App.css';
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
+import { getData } from './utils/data.utils';
+
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+};
 
 const App = () => {
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [searchField, setSearchField] = useState('');
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLowerCase();
-    setSearchField(searchFieldString);
-  };
-
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => {
-        setMonsters(users);
-      });
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      setMonsters(users);
+    };
+    fetchUsers();
   }, []);
 
   useEffect(() => {
@@ -28,6 +32,11 @@ const App = () => {
     );
     setFilteredMonsters(newFilteredMonsters);
   }, [monsters, searchField]);
+
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+  };
 
   return (
     <div className='App'>
---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,23 @@
+import { ChangeEventHandler } from 'react';
+import './search-box.styles.css';
+
+type searchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+};
+
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+}: searchBoxProps) => (
+  <input
+    className={`search-box ${className}`}
+    type='search'
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
